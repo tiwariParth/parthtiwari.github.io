0e5271d4d54a52aba6203e2a4c35f0004744321e
Bingolast <243mjparth@gmail.com>
2023-08-23 08:20:00 +0530

    updated favicon and basic navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,6 +3,7 @@ import { Link } from "react-router-dom";
 import { styles } from "../style";
 import { navLinks } from "../constants";
 import { logo, menu, close } from "../assets";
+import { act } from "@react-three/fiber";
 
 const Navbar = () => {
   const [active, setActive] = useState(" ");
@@ -10,7 +11,7 @@ const Navbar = () => {
     <nav
       className={`${styles.paddingX} w-full flex items-center py-5 fixed top-0 z-20 bg-primary`}
     >
-      <div className="w-full flex justuify-between items-center max-w-7xl mx-auto">
+      <div className="w-full flex justify-between items-center max-w-7xl mx-auto">
         <Link
           to={"/"}
           className="flex items-center gap-2"
@@ -24,6 +25,22 @@ const Navbar = () => {
             Parth <span className="sm:block hidden">| Tiwari</span>
           </p>
         </Link>
+        <ul className="list-none hidden sm:flex flex-row gap-10 ">
+          {navLinks.map((nav) => (
+            <li
+              key={nav.id}
+              className={`${
+                active === nav.title ? "text-white" : "text-secondary"
+              } hover:text-white text-[18px] font-medium cursor-pointer`}
+              onClick={() => setActive(nav.title)}
+            >
+              <a href={`#${nav.id}`}> {nav.title} </a>
+            </li>
+          ))}
+        </ul>
+        <div className="">
+
+        </div>
       </div>
     </nav>
   );
