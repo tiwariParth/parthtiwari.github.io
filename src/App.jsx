f3cd9fc4c56c9620364d597e1100c0b123945bc4
Bingolast <243mjparth@gmail.com>
2023-08-23 07:10:37 +0530

    Update codebase to use BrowserRouter and add missing components.

--- src/App.jsx
+++ src/App.jsx
@@ -12,7 +12,25 @@ import {
   StarsCanvas,
 } from "./components";
 const App = () => {
-  return <div>App</div>;
+  return (
+    <BrowserRouter>
+      <div className="relative z-0 bg-primary">
+        <div className="bg-hero-pattern bg-cover bg-no-repeat bg-center">
+          <Navbar />
+          <Hero />
+        </div>
+        <About />
+        <Experience />
+        <Tech />
+        <Works />
+        <Feedbacks />
+        <div className="relative z-0">
+          <Contact />
+          <StarsCanvas />
+        </div>
+      </div>
+    </BrowserRouter>
+  );
 };
 
 export default App;
