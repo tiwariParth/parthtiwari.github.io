c8716365be1cf355598c4e93f4a8754bcb10ced2
Bingolast <243mjparth@gmail.com>
2023-08-23 07:57:25 +0530

    upadated logo and added to favicon

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,7 +5,28 @@ import { navLinks } from "../constants";
 import { logo, menu, close } from "../assets";
 
 const Navbar = () => {
-  return <div></div>;
+  const [active, setActive] = useState(" ");
+  return (
+    <nav
+      className={`${styles.paddingX} w-full flex items-center py-5 fixed top-0 z-20 bg-primary`}
+    >
+      <div className="w-full flex justuify-between items-center max-w-7xl mx-auto">
+        <Link
+          to={"/"}
+          className="flex items-center gap-2"
+          onClick={() => {
+            setActive("");
+            window.scrollTo(0, 0);
+          }}
+        >
+          <img src={logo} alt="logo" className="w-9 h-9 object-contains" />
+          <p className="text-white text-[18px] font-bold cursor-pointer ">
+            Parth <span className="sm:block hidden">| Tiwari</span>
+          </p>
+        </Link>
+      </div>
+    </nav>
+  );
 };
 
 export default Navbar;
