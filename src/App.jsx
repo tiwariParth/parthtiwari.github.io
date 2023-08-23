c253ff102483c53e81f690a9963145a118e1cfa3
Bingolast <243mjparth@gmail.com>
2023-08-23 07:07:06 +0530

    Update HTML and JS files

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,18 @@
-import React from 'react'
-
+import React from "react";
+import { BrowserRouter } from "react-router-dom";
+import {
+  About,
+  Contact,
+  Experience,
+  Feedbacks,
+  Hero,
+  Navbar,
+  Tech,
+  Works,
+  StarsCanvas,
+} from "./components";
 const App = () => {
-  return (
-    <div>App</div>
-  )
-}
+  return <div>App</div>;
+};
 
-export default App
+export default App;
