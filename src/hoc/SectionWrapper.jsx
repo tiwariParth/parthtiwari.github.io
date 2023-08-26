803b2a0c29d42b88782668935fbc7e4cb0c59bab
Bingolast <243mjparth@gmail.com>
2023-08-26 08:00:53 +0530

    Added services section with tilt animation

---
+++ src/hoc/SectionWrapper.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { motion } from "framer-motion";
+import { styles } from "../style";
+import { staggerContainer } from "../utils/motion";
+
+const SectionWrapper = (Component, idName) => function HOC() {
+    
+};
+
+export default SectionWrapper;
