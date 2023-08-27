eed4d53dac3ceb0582670c5adc971bcc1db53675
Bingolast <243mjparth@gmail.com>
2023-08-27 17:51:05 +0530

    just fixed a lil bug

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -41,14 +41,14 @@ const Navbar = () => {
         </ul>
         <div className="sm:hidden flex flex-1 justify-end items-center">
           <img
-            src={toggle ? menu : close}
+            src={toggle ? close : menu}
             alt="menu"
             className="w-[28px] h-[28px] object-contain cursor-pointer"
             onClick={() => setToggle((prev) => !prev)}
           />
           <div
             className={`${
-              !toggle ? "flex" : "hidden"
+              !toggle ? "hidden" : "flex"
             } p-6 black-gradient absolute top-20 right-0 mx-4 my-2 min-w-[140px] z-10 rounded-xl`}
           >
             <ul className="list-none flex justify-end items-start flex-col gap-4  ">
