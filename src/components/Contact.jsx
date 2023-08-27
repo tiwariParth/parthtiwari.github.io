2edd2f096e2385c8733dbad484fba12acf3e116d
Bingolast <243mjparth@gmail.com>
2023-08-27 16:15:10 +0530

    Update Contact and Works components

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -18,9 +18,27 @@ const Contact = () => {
 
   const handleChange = (e) => {};
   const handleSubmit = (e) => {};
-  return <div className="">
-
-  </div>;
+  return (
+    <div className="xl:mt-12 xl:flex-row flex-col-reverse flex gap-10 overflow-hidden">
+      <motion.div
+        variants={slideIn("left", "tween", 0.2, 1)}
+        className="flex-[0.75] bg-black-100 p-8 rounded-2xl"
+      >
+        <p className={styles.sectionSubText}>Get In Touch</p>
+        <h3 className={styles.sectionHeadText}>Contact</h3>
+        <form
+          ref={formref}
+          onSubmit={handleSubmit}
+          className="mt-12 flex flex-col gap-8"
+        >
+          <label className="flex flex-col">
+            <span className="text-white font-medium mb-4">Your Name</span>
+            <input type="text" name="name" value={form.name} />
+          </label>
+        </form>
+      </motion.div>
+    </div>
+  );
 };
 
-export default Contact;
+export default SectionWrapper(Contact, "contact");
